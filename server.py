ab31c7d8f885ab1236ff8351509e951e1d6f7a62
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 <aaashwin515@gmail.com>
2021-09-09 23:55:57 +0530

    Added admin interface

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app=Flask(__name__,template_folder='template')
 
 @app.route('/')
 def customerlogin():
-    return render_template('/admin interface/adminlogin.html')
+    return render_template('/admin interface/admininbox.html')
 
 if __name__=="__main__":
     app.run(debug=True)
