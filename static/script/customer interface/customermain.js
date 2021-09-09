b322e92bc0b30b28c64369a74bfd7a847d759f53
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 <aaashwin515@gmail.com>
2021-09-09 20:13:23 +0530

    Added customer interface

---
+++ static/script/customer interface/customermain.js
@@ -0,0 +1,8 @@
+function helponclick(){
+    document.querySelector('.main-slide').style.display="none";
+    document.querySelector('.help-slide').style.display="block";
+}
+function helponbackclick(){
+    document.querySelector('.main-slide').style.display="block";
+    document.querySelector('.help-slide').style.display="none";
+}
